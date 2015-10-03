1a1567eee37652bc3d591985da72e11828a48aa6
chris <chris@yobetit.com>
2015-10-03 19:37:49 +0200

    added exception handler

--- sendgrid-playground-rest-service/src/main/java/com/genericmethod/sendgridplayground/SendGridEventNotificationController.java
+++ sendgrid-playground-rest-service/src/main/java/com/genericmethod/sendgridplayground/SendGridEventNotificationController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -12,6 +13,11 @@ import java.util.List;
 
 @RestController
 public class SendGridEventNotificationController {
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> exception(HttpServletRequest req, Exception e){
+        return new ResponseEntity<>("NOT OK - "+e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
     @RequestMapping(name = "/events", method = RequestMethod.POST)
     public ResponseEntity<String> event(@RequestBody List<String> sendGridEventNotification){
