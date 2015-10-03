d5f2674bb46f196c46a78321dbcadd02d22b5bb9
chris <chris@yobetit.com>
2015-10-03 19:57:29 +0200

    added logging

--- sendgrid-playground-rest-service/src/main/java/com/genericmethod/sendgridplayground/SendGridEventNotificationController.java
+++ sendgrid-playground-rest-service/src/main/java/com/genericmethod/sendgridplayground/SendGridEventNotificationController.java
@@ -1,5 +1,7 @@
 package com.genericmethod.sendgridplayground;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +16,18 @@ import java.util.List;
 @RestController
 public class SendGridEventNotificationController {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<String> exception(HttpServletRequest req, Exception e){
+        log.info("Exception thrown");
         return new ResponseEntity<>("NOT OK - "+e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @RequestMapping(name = "/events", method = RequestMethod.POST)
     public ResponseEntity<String> event(@RequestBody String[] sendGridEventNotification){
-        return new ResponseEntity<>("OK - "+ sendGridEventNotification.toString(), HttpStatus.OK);
+        log.info("Event received");
+        return new ResponseEntity<>("OK - "+ sendGridEventNotification, HttpStatus.OK);
     }
 
 }
